ad527ff2098f62151b1094ad290cdf0eb8355770
PeyMuz <kbantog1@gmail.com>
2026-09-22 17:20:16 +0800

    Done coding mazes.py

--- mazes.py
+++ mazes.py
@@ -47,3 +47,34 @@ maze_level_1: list[str] = [
     "X X X X X X X X X X X X X X X X X X X X X X X X X X X X",
     "X X X X X X X X X X X X X X X X X X X X X X X X X X X X"
 ]
+
+def calculate_maze_data(maze_level: list[str]) -> tuple[list, list, list]:
+    "Setup maze level"
+    walls: list = []
+    pellets: list = []
+    power_pellets: list = []
+
+    #Iterate over each row of the maze level
+
+    for row in range(MAZE_GRID_ROWS):
+    # Iterate over each column of the maze level
+        for column in range(MAZE_GRID_COLUMNS):
+            # Store the coordinates of the character[row][column]
+            character = maze_level[row][column]
+
+            # row index starts from 0 to 36 (37 not included)
+            character_x: float = MAZE_LEVEL_START_X + CELL_SIZE * column
+            character_y: float = MAZE_LEVEL_START_Y + CELL_SIZE * row
+
+            if character == "X":
+                # append the coordinate of that wall to the walls list
+                walls.append((character_x, character_y))
+            elif character == ".":
+                    pellets.append((character_x, character_y))
+            elif character == "0":
+                    power_pellets.append((character_x, character_y))
+# Return the list with all the coordinates
+#This allows us later to get this data in other files and store it as a variable
+
+    return walls, pellets, power_pellets
+        
